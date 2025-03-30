9bfb25bed4b5a50c543c27cb4e8ed0b013c9db3d
Tom Leyrie <Tom.leyrie@gmail.com>
2025-03-30 12:39:14 -0400

    tp3 exercice 1-2

---
+++ 3-Textures/Assets/Exercice/1/TreeShader.shader
@@ -0,0 +1,52 @@
+﻿// UNITY_SHADER_NO_UPGRADE
+
+Shader "8TRD150/Tex_0_Tree"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            sampler2D _MainTex;
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+                float2 uv : TEXCOORD0;
+            };
+            
+            float4x4 modelMatrix;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                float4x4 mvp = mul(UNITY_MATRIX_VP, modelMatrix);
+                o.vertex = mul(mvp, v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                return tex2D(_MainTex, i.uv);
+            }
+            ENDCG
+        }
+    }
+}
---
+++ 3-Textures/Assets/Exercice/2/TreeQuadController2.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class TreeQuadController2 : MonoBehaviour
+{
+    public float speed = 10.0f;
+    private float angle;
+
+    private Material material;
+
+    public void Start()
+    {
+        var mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        mesh.vertices = new Vector3[]
+        {
+            new Vector3(-0.5f, -0.5f, 0),
+            new Vector3(-0.5f, 0.5f, 0),
+            new Vector3(0.5f, 0.5f, 0),
+            new Vector3(0.5f, -0.5f, 0),
+        };
+
+        mesh.triangles = new int[] { 0, 1, 2, 2, 3, 0 };
+        
+        mesh.triangles = new int[] { 0, 1, 2, 2, 3, 0 };
+
+        mesh.uv = new Vector2[]
+        {
+            new Vector2(0, 0),
+            new Vector2(0, 1),
+            new Vector2(1, 1),
+            new Vector2(1, 0),
+        };
+
+        var meshRenderer = GetComponent<MeshRenderer>();
+        material = new Material(meshRenderer.sharedMaterial);
+        meshRenderer.material = material;
+    }
+
+    public void Update()
+    {
+        angle += Time.deltaTime * speed;
+        var modelView = Matrix4x4.Rotate(Quaternion.Euler(0, 0, angle));
+        material.SetMatrix("modelMatrix", modelView);
+    }
+}
